55cad18ecae2e866c3f51a9139be53078f62b3fb
rvsfrebeca1 <rebecaferreiraprofissional@gmail.com>
2022-04-26 14:53:28 -0300

    fist commit

---
+++ src/test/java/com/br/snsilab/SNS/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.snsilab.SNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
