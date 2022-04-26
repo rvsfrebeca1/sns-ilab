d01616f7b44e74a9102d117c16b8c69484389e1d
rvsfrebeca1 <rebecaferreiraprofissional@gmail.com>
2022-04-26 16:38:10 -0300

    first commit

---
+++ execute.sh
@@ -0,0 +1,1 @@
+java -jar ./target/sns-0.0.1-SNAPSHOT.jar
---
+++ src/test/java/br/com/sns_ilab/SnsApplicationTests.java
@@ -0,0 +1,19 @@
+package br.com.sns_ilab;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SnsApplicationTests{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
